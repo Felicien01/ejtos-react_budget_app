b5e49a6f8914e62f325ee4414836ef9238e27379
Your Name <you@example.com>
2023-03-20 09:39:31 -0400

    with buttons but not pretty ones

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,6 +1,8 @@
 import React, { useContext } from 'react';
 import { TiDelete } from 'react-icons/ti';
 import { AppContext } from '../context/AppContext';
+import { FaPlusCircle } from "react-icons/fa";
+import { FaMinusCircle } from "react-icons/fa";
 
 const ExpenseItem = (props) => {
     const {dispatch,currency} = useContext(AppContext);
@@ -41,8 +43,8 @@ const ExpenseItem = (props) => {
         <tr>
         <td>{props.name}</td>
         <td>{currency} {props.cost}</td>
-        <td><button onClick={event=> increaseAllocation(props.name)}></button></td>
-        <td><button onClick={event=> decreaseAllocation(props.name)}>-</button></td>
+        <td><button onClick={event=> increaseAllocation(props.name)}><FaPlusCircle color='green'/></button></td>
+        <td><button onClick={event=> decreaseAllocation(props.name)}><FaMinusCircle color='red'/></button></td>
         <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
         </tr>
     );
