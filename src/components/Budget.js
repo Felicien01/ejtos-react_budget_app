278f8506cffc36a8d6bef9efb56290eba0f3dd40
Your Name <you@example.com>
2023-03-20 09:18:04 -0400

    before icons

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,11 +1,22 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import { AppContext } from '../context/AppContext';
+
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const {currency}=useContext(AppContext);
+    const [budget, setBudget] = useState("");
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            <span>Budget: {currency}</span>
+            <input
+                        required='required'
+                        type='number'
+                        id='budget'
+                        value={budget}
+                        style={{ marginLeft: '2rem' , size: 10}}
+                        onChange={(event) => setBudget(event.target.value)}>
+                        </input>
         </div>
     );
 };
+
 export default Budget;
